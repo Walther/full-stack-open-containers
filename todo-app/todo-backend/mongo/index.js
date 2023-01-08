76d003c6d5c6d81ffd7320dd11c064c05b7fcedd
Walther <veeti.haapsamo@gmail.com>
2023-01-08 20:04:18 +0200

    Solve exercise 12.16

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
+const { MONGO_URL } = require("../util/config");
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(MONGO_URL);
+if (MONGO_URL && !mongoose.connection.readyState)
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+  });
 
+console.log(mongoose.connection.readyState);
 
 module.exports = {
-  Todo
-}
+  Todo,
+};
